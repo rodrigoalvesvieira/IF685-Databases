2b9afcd1900d93631db1dfbb2f772a2236d122b2
Rodrigo Alves <rodrigo.alves@toptal.com>
2015-10-10 17:30:42 -0300

    one more

--- List-SQL-PL/answers-SQL.sql
+++ List-SQL-PL/answers-SQL.sql
@@ -9,3 +9,7 @@ inner join
   on F.cpf = TF.funcionario_cpf
 where
   F.cpf_super = '06234438765' AND auth_token LIKE '%9' ORDER BY nome ASC;
+
+-- 2
+-- status: pending
+SELECT COUNT(*) FROM embarcacoes WHERE capacidade_carga BETWEEN 50.000 AND 100.000 GROUP BY tipo;
