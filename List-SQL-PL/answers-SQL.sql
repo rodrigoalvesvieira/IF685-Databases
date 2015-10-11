97108fd0a008d2e73cafb3b4bced60e3d8dc1f10
Rodrigo Alves <rodrigo.alves@toptal.com>
2015-10-11 10:22:55 -0300

    One more: the 5th. Several fixes.

--- List-SQL-PL/answers-SQL.sql
+++ List-SQL-PL/answers-SQL.sql
@@ -1,33 +1,41 @@
 -- 1
--- status: pending
-SELECT
-  cpf, nome
-FROM
-  funcionarios F
-inner join
-  telefones_funcionarios TF
-  on F.cpf = TF.funcionario_cpf
-where
-  F.cpf_super = '06234438765' AND auth_token LIKE '%9' ORDER BY nome ASC;
+-- status: accepted
+SELECT F.cpf, F.nome
+FROM funcionarios F
+INNER JOIN telefones_funcionarios TF ON F.cpf = TF.funcionario_cpf
+WHERE
+  F.cpf_super = '06234438765' AND TF.fone LIKE '%9' ORDER BY F.nome ASC;
 
 -- 2
--- status: pending
-SELECT COUNT(*) FROM embarcacoes WHERE capacidade_carga BETWEEN 50.000 AND 100.000 GROUP BY tipo;
+-- status: accepted
+SELECT COUNT(EMB.certificado), EMB.tipo
+FROM embarcacoes EMB, especificacoes ESP
+WHERE ESP.certificado_embarcacao = EMB.certificado
+AND ESP.capacidade_carga BETWEEN 50.000 AND 100.000
+GROUP BY EMB.tipo;
 
 -- 4
 -- status: pending
 CREATE USER novo_usuario IDENTIFIED BY quero_passar_direto;
 GRANT SELECT ON funcionarios TO novo_usuario;
 
+-- 5
+-- status: accepted
+SELECT COUNT(CE.equipamento_id), E.tipo
+FROM compras_equipamentos CE, equipamentos E
+WHERE CE.equipamento_id = E.id
+GROUP BY E.tipo
+HAVING COUNT(CE.equipamento_id) >= 2;
+
 -- 7
--- status: pending
+-- status: accepted
 SELECT CL.site, REP.nome
-FROM clientes CL, representantes REP
-WHERE REP.empresa_cnpj = CL.cnpj
-AND CL.site LIKE '%com' OR CL.site LIKE '%com.br'
+FROM clientes CL
+INNER JOIN representantes REP ON CL.cnpj = REP.empresa_cnpj
+WHERE CL.site LIKE '%.com' OR CL.site LIKE '%.com.br';
 
 -- 9
--- status: pending
+-- status: accepted
 SELECT
   MAX(SumPesos)
 FROM (
@@ -35,4 +43,4 @@ FROM (
   FROM especificacoes ESP, embarcacoes EMB
   WHERE ESP.certificado_embarcacao = EMB.certificado
   GROUP BY EMB.tipo
-)
+);
