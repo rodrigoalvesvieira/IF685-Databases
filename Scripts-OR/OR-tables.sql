751309a50376f92caa7b23ba710f91b19b8539f0
Rodrigo Alves <rodrigo.alves@toptal.com>
2015-11-06 08:05:25 -0300

    ch ch changes

--- Scripts-OR/OR-tables.sql
+++ Scripts-OR/OR-tables.sql
@@ -1,71 +1,71 @@
 /* Drop tables */
-DROP TABLE funcionarios CASCADE CONSTRAINTS;
-DROP TABLE telefones_funcionarios CASCADE CONSTRAINTS;
-DROP TABLE enderecos_funcionarios CASCADE CONSTRAINTS;
-DROP TABLE clientes CASCADE CONSTRAINTS;
-DROP TABLE representantes CASCADE CONSTRAINTS;
-DROP TABLE telefones_clientes CASCADE CONSTRAINTS;
-DROP TABLE enderecos_clientes CASCADE CONSTRAINTS;
-DROP TABLE embarcacoes CASCADE CONSTRAINTS;
-DROP TABLE especificacoes CASCADE CONSTRAINTS;
-DROP TABLE responsaveis_setores CASCADE CONSTRAINTS;
-DROP TABLE setores CASCADE CONSTRAINTS;
+DROP TABLE tb_funcionarios CASCADE CONSTRAINTS;
+DROP TABLE tb_telefones_funcionarios CASCADE CONSTRAINTS;
+DROP TABLE tb_enderecos_funcionarios CASCADE CONSTRAINTS;
+DROP TABLE tb_clientes CASCADE CONSTRAINTS;
+DROP TABLE tb_representantes CASCADE CONSTRAINTS;
+DROP TABLE tb_telefones_clientes CASCADE CONSTRAINTS;
+DROP TABLE tb_enderecos_clientes CASCADE CONSTRAINTS;
+DROP TABLE tb_embarcacoes CASCADE CONSTRAINTS;
+DROP TABLE tb_especificacoes CASCADE CONSTRAINTS;
+DROP TABLE tb_responsaveis_setores CASCADE CONSTRAINTS;
+DROP TABLE tb_setores CASCADE CONSTRAINTS;
 
-/* DROP TABLE projetos FORCE; tirei, visto que deixou de existir. Agora inserimos direto nas tabelas de especializações usando somente o tipo_projetos */
+/* DROP TABLE tb_projetos FORCE; tirei, visto que deixou de existir. Agora inserimos direto nas tabelas de especializações usando somente o tipo_projetos */
 
 DROP TABLE projetos_criacoes CASCADE CONSTRAINTS;
 DROP TABLE projetos_restauracoes CASCADE CONSTRAINTS;
 DROP TABLE execucoes_projetos CASCADE CONSTRAINTS;
 DROP TABLE solicitacoes_projetos CASCADE CONSTRAINTS;
-DROP TABLE equipamentos CASCADE CONSTRAINTS;
+DROP TABLE tb_equipamentos CASCADE CONSTRAINTS;
 DROP TABLE compras_equipamentos CASCADE CONSTRAINTS;
 DROP TABLE manutencoes_equipamentos CASCADE CONSTRAINTS;
 
 /* Create tables */
-CREATE TABLE funcionarios OF tipo_funcionarios (
+CREATE TABLE tb_funcionarios OF tipo_funcionarios (
   cpf PRIMARY KEY,
-  cpf_super WITH ROWID REFERENCES funcionarios
+  cpf_super WITH ROWID REFERENCES tb_funcionarios
 );
 
-CREATE TABLE telefones_funcionarios (
+CREATE TABLE tb_telefones_funcionarios (
   funcionario_cpf REF tipo_funcionarios,
   lista_fones tipo_telefones_funcionarios
 );
 
-CREATE TABLE enderecos_funcionarios OF tipo_enderecos_funcionarios (
+CREATE TABLE tb_enderecos_funcionarios OF tipo_enderecos_funcionarios (
   cep PRIMARY KEY,
-  funcionario_cpf WITH rowid REFERENCES funcionarios
+  funcionario_cpf WITH rowid REFERENCES tb_funcionarios
 );
 
-CREATE TABLE clientes OF tipo_clientes (
+CREATE TABLE tb_clientes OF tipo_clientes (
   cnpj PRIMARY KEY
 );
 
-CREATE TABLE lista_telefones_clientes (
+CREATE TABLE tb_telefones_clientes (
   cliente_cnpj REF tipo_clientes,
   lista_fones tipo_nt_telefones_clientes
 ) nested TABLE lista_fones store AS nt_lista_fones;
 
-CREATE TABLE representantes OF tipo_representantes (
+CREATE TABLE tb_representantes OF tipo_representantes (
   cpf_representante PRIMARY KEY,
-  empresa_cnpj WITH rowid REFERENCES clientes
+  empresa_cnpj WITH rowid REFERENCES tb_clientes
 );
 
-CREATE TABLE enderecos_clientes OF tipo_enderecos_clientes (
+CREATE TABLE tb_enderecos_clientes OF tipo_enderecos_clientes (
   cep PRIMARY KEY,
-  cliente_cnpj WITH rowid REFERENCES clientes
+  cliente_cnpj WITH rowid REFERENCES tb_clientes
 );
 
-CREATE TABLE embarcacoes OF tipo_embarcacoes (
+CREATE TABLE tb_embarcacoes OF tipo_embarcacoes (
   certificado PRIMARY KEY
 );
 
-CREATE TABLE especificacoes OF tipo_especificacoes (
+CREATE TABLE tb_especificacoes OF tipo_especificacoes (
   versao PRIMARY KEY,
-  certificado_embarcacao WITH rowid REFERENCES embarcacoes
+  certificado_embarcacao WITH rowid REFERENCES tb_embarcacoes
 );
 
-CREATE TABLE setores OF tipo_setores (
+CREATE TABLE tb_setores OF tipo_setores (
   id PRIMARY KEY
 );
 
@@ -77,33 +77,33 @@ CREATE TABLE projetos_restauracoes OF tipo_projetos_restauracoes (
   id PRIMARY KEY
 );
 
-CREATE TABLE responsaveis_setores OF tipo_responsaveis_setores (
-  funcionario_cpf WITH rowid REFERENCES funcionarios,
-  setor_id WITH rowid REFERENCES setores
+CREATE TABLE tb_responsaveis_setores OF tipo_responsaveis_setores (
+  funcionario_cpf WITH rowid REFERENCES tb_funcionarios,
+  setor_id WITH rowid REFERENCES tb_setores
 );
 
 CREATE TABLE execucoes_projetos OF tipo_execucoes_projetos (
-  setor_id WITH rowid REFERENCES setores,
+  setor_id WITH rowid REFERENCES tb_setores,
   projeto_id WITH rowid REFERENCES projetos_criacoes
 );
 
 CREATE TABLE solicitacoes_projetos OF tipo_solicitacoes_projetos (
   projeto_id WITH rowid REFERENCES projetos_criacoes,
-  embarcacao_certificado WITH rowid REFERENCES embarcacoes,
-  cliente_cnpj WITH rowid REFERENCES clientes
+  embarcacao_certificado WITH rowid REFERENCES tb_embarcacoes,
+  cliente_cnpj WITH rowid REFERENCES tb_clientes
 );
 
-CREATE TABLE equipamentos OF tipo_equipamentos (
+CREATE TABLE tb_equipamentos OF tipo_equipamentos (
   id PRIMARY KEY
 );
 
 CREATE TABLE compras_equipamentos OF tipo_compras_equipamentos (
-  equipamento_id WITH rowid REFERENCES equipamentos,
-  setor_id WITH rowid REFERENCES setores,
-  funcionario_cpf WITH rowid REFERENCES funcionarios
+  equipamento_id WITH rowid REFERENCES tb_equipamentos,
+  setor_id WITH rowid REFERENCES tb_setores,
+  funcionario_cpf WITH rowid REFERENCES tb_funcionarios
 );
 
 CREATE TABLE manutencoes_equipamentos OF tipo_manutencoes_equipamentos (
-  data PRIMARY KEY,
-  equipamento_id WITH rowid REFERENCES equipamentos
+  data_manutencao PRIMARY KEY, /* renomeado, antes era só data */
+  equipamento_id WITH rowid REFERENCES tb_equipamentos
 );
